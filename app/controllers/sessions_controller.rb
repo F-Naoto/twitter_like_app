b7725fb98fd99d625c7a8d095002c26b8a345050
NaotoF <2016asap@gmail.com>
2022-05-07 23:19:25 +0900

    finish remember_digest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ def create
 user = User.find_by(email: params[:session][:email])
   if user && user.authenticate(params[:session][:password])
   log_in(user)
+  params[:session][:remember_me] == 1 ?remember(user) : forget(user)
   redirect_to user
   else
   render 'new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_132427) do
+ActiveRecord::Schema.define(version: 2022_05_07_003900) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_04_30_132427) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
   add_foreign_key "favorites", "posts"
